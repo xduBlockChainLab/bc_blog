e4f5eb6c3acbe9e6626c2f41781dbdc53421503c
zyp <1977689848@qq.com>
2022-05-22 16:33:06 +0800

    encode_password

--- bcblog-back/src/main/java/com/bc208/blog/controller/UserController.java
+++ bcblog-back/src/main/java/com/bc208/blog/controller/UserController.java
@@ -5,8 +5,15 @@ import com.bc208.blog.utils.ParamsException;
 import com.bc208.blog.utils.ResultInfo;
 import com.bc208.blog.pojo.UserModel;
 import com.bc208.blog.service.impl.UsersServiceImpl;
+import com.sun.org.apache.xerces.internal.impl.dv.util.HexBin;
+import org.apache.tomcat.util.buf.HexUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import org.apache.commons.codec.binary.Hex;
 
 @RestController
 @RequestMapping("/bc208/api")
@@ -16,13 +23,19 @@ public class UserController {
 
     @PostMapping("/login")
     @ResponseBody
-    public ResultInfo userLogin(@RequestBody LoginDto user){
+    public ResultInfo userLogin(@RequestBody LoginDto user) throws NoSuchAlgorithmException {
         ResultInfo resultInfo = new ResultInfo();
         System.out.println(user.getEmail() +" "+ user.getPassword());
         String email = user.getEmail();
         String password = user.getPassword();
+
+
+        MessageDigest messageDigest =MessageDigest.getInstance("SHA-256");
+        byte[] digest = messageDigest.digest(password.getBytes(StandardCharsets.UTF_8));
+        String private_password = Hex.encodeHexString(digest);
+
        try{
-            UserModel userModel= usersServiceImpl.userLogin(email,password);
+            UserModel userModel= usersServiceImpl.userLogin(email,private_password);
             resultInfo.setResult(userModel);
         }catch (ParamsException p){
            resultInfo.setCode(p.getCode());
@@ -38,12 +51,16 @@ public class UserController {
     @PostMapping("/register")
     @ResponseBody
 
-    public ResultInfo userRegister(@RequestBody LoginDto user){
+    public ResultInfo userRegister(@RequestBody LoginDto user) throws NoSuchAlgorithmException {
         ResultInfo resultInfo = new ResultInfo();
         String email = user.getEmail();
         String password = user.getPassword();
+
+        MessageDigest messageDigest =MessageDigest.getInstance("SHA-256");
+        byte[] digest = messageDigest.digest(password.getBytes(StandardCharsets.UTF_8));
+        String private_password = Hex.encodeHexString(digest);
         try{
-            UserModel userModel = usersServiceImpl.userRegister(email,password);
+            UserModel userModel = usersServiceImpl.userRegister(email,private_password);
             resultInfo.setResult(userModel);
         }catch (ParamsException p){
             resultInfo.setCode(p.getCode());
